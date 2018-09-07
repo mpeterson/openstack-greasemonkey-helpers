d1158db23235a16551c791573c0d877f83e69845
Sorin Sbarnea <ssbarnea@redhat.com>
2018-09-07 18:12:45 +0100

    Added pre-commit with eslint

--- src/openstack_zuul.user.js
+++ src/openstack_zuul.user.js
@@ -23,14 +23,14 @@
 
 function GM_addStyle(css) {
     const style = document.getElementById("GM_addStyleBy8626") || (function() {
-      const style = document.createElement('style');
-      style.type = 'text/css';
-      style.id = "GM_addStyleBy8626";
-      document.head.appendChild(style);
-      return style;
+        const style = document.createElement("style");
+        style.type = "text/css";
+        style.id = "GM_addStyleBy8626";
+        document.head.appendChild(style);
+        return style;
     })();
     const sheet = style.sheet;
     sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
-  }
-  
-  GM_addStyle(".zuul-patchset-header { cursor: pointer; }");
+}
+
+GM_addStyle(".zuul-patchset-header { cursor: pointer; }");
